2bb787827d45c4f625820467bb31a059bf3ea10a
rifatjahan823 <rifatjahan823@gmail.com>
2023-09-27 11:52:10 +0600

    first commit

---
+++ src/router/router.tsx
@@ -0,0 +1,47 @@
+import  { createBrowserRouter} from'react-router-dom'
+import Home from '@/pages/Home'
+import Products from '@/pages/Products'
+import ProductDetails from '@/pages/ProductDetails'
+import Checkout from '@/pages/Checkout'
+import Login from '@/pages/Login'
+import Signup from '@/pages/Signup'
+import NotFound from '@/pages/NotFound'
+import App from '@/App'
+
+
+  export const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App/>,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: '/products',
+          element: <Products />,
+        },
+        {
+          path: '/product-details/:id',
+          element: <ProductDetails />,
+        },
+        {
+          path: '/checkout',
+          element: <Checkout />,
+        },
+      ],
+    },
+    {
+      path: '/login',
+      element: <Login />,
+    },
+    {
+      path: '/signup',
+      element: <Signup />,
+    },
+    {
+      path: '*',
+      element: <NotFound />,
+    },
+  ])
